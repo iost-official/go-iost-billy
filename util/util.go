082f646613788c6f884c21090cf6a4389c48c305
deBreaker <wang.yannan@outlook.com>
2018-04-02 18:14:39 +0800

    add copyfile func

--- util/util.go
+++ util/util.go
@@ -183,3 +183,51 @@ func getUnderlyingAndPath(fs billy.Basic, path string) (billy.Basic, string) {
 
 	return u.Underlying(), path
 }
+
+// CopyFile copies a file across filesystems
+func CopyFile(src, dst Filesystem, srcPath, dstPath string) error {
+	srcFile, err := src.Open(srcPath)
+	if err != nil {
+		return err
+	}
+
+	dstFile, err := dst.Create(dstPath)
+	if err != nil {
+		_ = srcFile.Close()
+		_ = dst.Remove(dstPath)
+		return err
+	}
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		_ = dstFile.Close()
+		_ = dst.Remove(dstPath)
+		_ = srcFile.Close()
+		return err
+	}
+
+	if err := srcFile.Close(); err != nil {
+		_ = dstFile.Close()
+		_ = dst.Remove(dstPath)
+		return err
+	}
+
+	err = dstFile.Close()
+	if err != nil {
+		_ = dst.Remove(dstPath)
+		_ = srcFile.Close()
+		return err
+	}
+
+	return nil
+}
+
+// Exists returns true if the path exists in the filesystem.
+func Exists(fs Filesystem, path string) (bool, error) {
+	_, err := fs.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return err == nil, err
+}
